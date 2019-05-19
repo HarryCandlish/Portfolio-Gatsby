ed3bdc68fcc59f0aa1732ba3b530d0378ce4124f
HarryCandlish <hcnd90@gmail.com>
2019-05-20 10:51:15 +1200

    changes to files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Harry Candlish</h1>
+      <h2>Full-Stack Developer</h2>
+      <Link to="/contact">Contact</Link>
+    </div>
+  )
+}
+
+export default IndexPage
